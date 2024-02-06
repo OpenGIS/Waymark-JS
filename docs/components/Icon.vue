697bef150c5eab73bbdeba3f46d3ccf7f5f7d565
Joe Hawes <morehawes@gmail.com>
2024-02-06 13:16:43 -0800

    Docs restructure

---
+++ docs/components/Icon.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+  name: { default: "logo", type: String },
+  color: { default: "#fff", type: String },
+  width: { default: "30", type: String },
+  height: { default: "30", type: String },
+});
+
+const getStyle = () => {
+  return `
+    background-color: ${props.color};
+    color: ${props.color};
+    mask-image: url(/assets/icon/${props.name}.svg);
+    width: ${props.width}px;
+    height: ${props.height}px;
+    mask-size: ${props.width}px ${props.height}px;
+  `;
+};
+</script>
+
+<template>
+  <div class="mini-icon-wrap">
+    <div class="mini-icon" :style="getStyle()" />
+
+    <slot />
+  </div>
+</template>
+
+<style lang="less">
+.mini-icon-wrap {
+  display: inline-block;
+  overflow: hidden;
+  .mini-icon {
+    display: inline-block;
+    mask-repeat: no-repeat;
+    min-width: 32px;
+    min-height: 32px;
+    mask-position: center;
+    overflow: hidden;
+  }
+}
+</style>
