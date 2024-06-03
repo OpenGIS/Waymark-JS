59b342db927bcc65b6893f607b2d7de2a2ecedf2
Joe Hawes <morehawes@gmail.com>
2024-06-03 11:59:22 -0700

    2

--- vite.site.config.ts
+++ vite.site.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  build: {
+    outDir: resolve(__dirname, "dist/site"),
+    emptyOutDir: true,
+  },
+});
