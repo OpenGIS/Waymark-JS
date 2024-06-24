086c9520d4debfa14107ef40846c94273fe1a6ad
Joe Hawes <morehawes@gmail.com>
2024-06-24 10:35:03 -0700

    useWaymark working

--- library/export.js
+++ library/export.js
@@ -1,3 +1,28 @@
-import { useWaymark } from "@/composables/useWaymark.js";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-export { useWaymark };
+import Instance from "../library/components/Instance.vue";
+
+export function useWaymark() {
+	const createInstance = (config) => {
+		// Ensure we at least have an ID
+		if (!config.id) {
+			console.error("No ID provided for Waymark instance.");
+			return;
+		}
+
+		// Create Instance
+		const instance = createApp(Instance, config);
+
+		// Add Pinia
+		const pinia = createPinia();
+		instance.use(pinia);
+
+		// Mount to DOM
+		instance.mount("#" + config.id);
+	};
+
+	return {
+		createInstance,
+	};
+}
