750fe0884e72f443e04a963984cd034659325903
Joe Hawes <morehawes@gmail.com>
2024-07-01 09:50:48 -0700

    4

--- library/components/UI/Panel/Overlay.vue
+++ library/components/UI/Panel/Overlay.vue
@@ -1,6 +1,25 @@
 <script setup>
+import { ref, computed } from "vue";
+import { overlaysByType } from "@/helpers/Overlay.js";
+
+import { storeToRefs } from "pinia";
+import { useInstanceStore } from "@/stores/instanceStore.js";
+
 import Detail from "@/components/UI/Panel/Overlay/Detail.vue";
 import List from "@/components/UI/Panel/Overlay/List.vue";
+
+const instanceStore = useInstanceStore();
+const { overlays, visibleOverlays } = storeToRefs(instanceStore);
+
+const activeType = ref("marker");
+
+const activeOverlays = computed(() => {
+	return overlaysByType(
+		visibleOverlays.value.filter((o) => {
+			return o.featureType === activeType.value;
+		}),
+	);
+});
 </script>
 
 <template>
@@ -8,7 +27,15 @@ import List from "@/components/UI/Panel/Overlay/List.vue";
 		<div class="panel-content">
 			<Detail />
 
-			<List />
+			<!-- Type Nav -->
+			<!-- <nav id="type-nav" :value="activeType">
+      <Button icon="fa-location-arrow" @click="activeType = 'marker'" />
+      <Button icon="fa-location-arrow" @click="activeType = 'line'" />
+      <Button icon="fa-location-arrow" @click="activeType = 'shape'" />
+    </nav> -->
+
+			<!-- Overlays (by Type) -->
+			<List :overlaysByType="overlaysByType(overlays)" />
 		</div>
 	</div>
 </template>
