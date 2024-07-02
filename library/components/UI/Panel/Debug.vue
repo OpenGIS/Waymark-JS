470106efe00f1bf7b8316a7455bf0d4668565056
Joe Hawes <morehawes@gmail.com>
2024-07-02 08:30:27 -0700

    2

--- library/components/UI/Panel/Debug.vue
+++ library/components/UI/Panel/Debug.vue
@@ -1,42 +1,45 @@
 <script setup>
-import { computed } from "vue";
+import { ref, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useInstanceStore } from "@/stores/instanceStore.js";
 
 const instanceStore = useInstanceStore();
 const { visibleOverlays } = storeToRefs(instanceStore);
 
+const activeType = ref("line");
+
 const debugContent = computed(() => {
-	/*
-
-			id: overlays.value.length + 1,
-			typeKey: typeKey,
-			typeData: getTypeData("marker", typeKey),
-			feature: feature,
-			marker: marker,
-			featureType: featureType,
-			element: markerElement,
-			imageURLs: getImageURLs(feature.properties),
-
-*/
-
-	return visibleOverlays.value.map((o) => {
-		return {
-			id: o.id,
-			typeKey: o.typeKey,
-			typeData: o.typeData,
-			feature: o.feature,
-			// marker: o.marker,
-			featureType: o.featureType,
-			// element: o.element,
-			imageURLs: o.imageURLs,
-		};
-	});
+	// Visible Overlays filtered by activeType
+	return visibleOverlays.value
+		.filter((o) => {
+			return o.featureType === activeType.value;
+		})
+		.map((o) => {
+			return {
+				id: o.id,
+				typeKey: o.typeKey,
+				typeData: o.typeData,
+				feature: o.feature,
+				// marker: o.marker,
+				featureType: o.featureType,
+				// element: o.element,
+				imageURLs: o.imageURLs,
+			};
+		});
 });
 </script>
 
 <template>
 	<div class="panel debug">
+		<!-- Filter by featureType -->
+		<nav>
+			<select v-model="activeType">
+				<option value="marker">Marker</option>
+				<option value="line">Line</option>
+				<option value="shape">Shape</option>
+			</select>
+		</nav>
+
 		<div class="panel-content">
 			<pre>{{ debugContent }}</pre>
 		</div>
