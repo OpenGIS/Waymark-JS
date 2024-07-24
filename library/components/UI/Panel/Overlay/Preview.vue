6774a2b48702588090d7270cd1efda9b0bdf7654
Joe Hawes <morehawes@gmail.com>
2024-07-24 09:52:22 -0700

    Preview basics

--- library/components/UI/Panel/Overlay/Preview.vue
+++ library/components/UI/Panel/Overlay/Preview.vue
@@ -1,5 +1,7 @@
 <script setup>
-defineProps({
+import { onMounted } from "vue";
+
+const props = defineProps({
   featureType: {
     type: String,
     required: true,
@@ -9,10 +11,15 @@ defineProps({
     required: true,
   },
 });
+
+onMounted(() => {
+  console.log("featureType", props);
+});
 </script>
 
 <template>
   <div class="preview">
+    <!-- Marker -->
     <div class="marker" v-if="featureType === 'marker'">
       <!-- Icon -->
       <div
@@ -22,5 +29,23 @@ defineProps({
         :style="`width:${typeData.iconData.iconSize[0]}px;height:${typeData.iconData.iconSize[1]}px`"
       />
     </div>
+
+    <!-- Line -->
+    <div class="line" v-else-if="featureType === 'line'">
+      <div
+        class="inner"
+        :style="`background-color:${typeData.line_colour};height:${typeData.line_weight}px`"
+      />
+    </div>
   </div>
 </template>
+
+<style lang="less">
+.preview {
+  .line {
+    .inner {
+      width: 100%;
+    }
+  }
+}
+</style>
