c572d5131a34da9e401e63322ea711275be79a3e
Joe Hawes <morehawes@gmail.com>
2025-05-11 14:57:11 -0700

    8 - state.width & state.height

--- library/stores/instanceStore.js
+++ library/stores/instanceStore.js
@@ -5,6 +5,22 @@ import { getTypeData, getImageURLs } from "@/helpers/Overlay.js";
 import { makeKey, deepMerge } from "@/helpers/Common.js";
 
 export const useInstanceStore = defineStore("instance", () => {
+	/*
+map_div_id  string  The ID of the HTML element to contain the Map. Defaults to waymark-map. map
+map_height  number  Specify the desired height of the Map (in pixels).  420
+map_width number  Specify the desired width of the Map (in pixels). 800
+map_init_zoom ==> zoom
+map_init_latlng ==> center
+map_init_basemap  string  The initial basemap of the Map. Use the exact title, including spaces.  Satellite Imagery
+map_max_zoom  0-18  The maximum zoom level of the Map.  12
+show_scale  1/0 Whether to show the scale on the Map. 1
+tile_layers array An array of Basemaps to be used on the Map. See Below
+marker_types  array An array of Marker Types to be used on the Map. See Below
+line_types  array An array of Line Types to be used on the Map. See Below
+shape_types array An array of Shape Types to be used on the Map.  See Below
+debug_mode  1/0 Whether to enable debug mode. This will output debug information to the console.  1
+*/
+
 	const defaultConfig = {
 		geoJSON: {},
 		map_options: {
@@ -27,6 +43,8 @@ export const useInstanceStore = defineStore("instance", () => {
 	const state = ref({
 		geoJSON: {},
 		container: {},
+		width: 0,
+		height: 0,
 	});
 
 	// Default Tile Layer
@@ -41,9 +59,6 @@ export const useInstanceStore = defineStore("instance", () => {
 
 	const activePanel = ref("overlays");
 	const panelOpen = ref(true);
-
-	const width = ref(0);
-	const height = ref(0);
 
 	function createStore(initConfig = {}) {
 		// Create a merged config
@@ -62,8 +77,10 @@ export const useInstanceStore = defineStore("instance", () => {
 
 		// Inital Dimensions
 		const getDimensions = () => {
-			width.value = state.value.container.clientWidth;
-			height.value = state.value.container.clientHeight;
+			state.value.width = state.value.container.clientWidth;
+			state.value.height = state.value.container.clientHeight;
+
+			console.log(`Width: ${state.value.width}, Height: ${state.value.height}`);
 		};
 		getDimensions();
 
@@ -318,12 +335,12 @@ export const useInstanceStore = defineStore("instance", () => {
 	});
 
 	const orientation = computed(() => {
-		return width.value > height.value ? "landscape" : "portrait";
+		return state.value.width > state.value.height ? "landscape" : "portrait";
 	});
 
 	const small = computed(() => {
 		// Small / Medium / Large
-		if (width.value <= 320) {
+		if (state.value.width <= 320) {
 			return "small";
 		}
 	});
@@ -355,8 +372,6 @@ export const useInstanceStore = defineStore("instance", () => {
 
 		// - Map Options
 
-		width,
-		height,
 		//lat
 		//lng
 		//zoom
