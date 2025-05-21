3f095110783b89da3eb69bd8256006e548502996
Joe Hawes <morehawes@gmail.com>
2025-05-21 13:48:19 -0700

    1 - Instance Store restructure basics

--- library/stores/instanceStore.js
+++ library/stores/instanceStore.js
@@ -5,21 +5,35 @@ import L from "leaflet";
 import { deepMerge } from "@/helpers/Common.js";
 
 export const useInstanceStore = defineStore("instance", () => {
-	// === CONFIGURATION ===
-
-	const defaultConfig = {
-		geoJSON: {},
-		map_options: {
-			div_id: "map",
-			leaflet_options: {
-				center: [51.1788144, -1.8261632],
-				zoom: 17,
-				attributionControl: false,
-			},
-		},
-	};
-
+	// State
 	const config = markRaw({});
+	const mapReady = ref(false);
+
+	// Getters
+
+	// Actions
+	function init(initConfig = {}) {
+		// Parse config & set defaults
+		const parsedConfig = deepMerge(
+			structuredClone({
+				geoJSON: {},
+				map_options: {
+					div_id: "map",
+					leaflet_options: {
+						center: [51.1788144, -1.8261632],
+						zoom: 17,
+						attributionControl: false,
+					},
+				},
+			}),
+			initConfig,
+		);
+
+		// Set config
+		for (const key in parsedConfig) {
+			config[key] = parsedConfig[key];
+		}
+	}
 
 	// === STATE ===
 
@@ -54,23 +68,16 @@ export const useInstanceStore = defineStore("instance", () => {
 		activeFeatureType: "marker",
 	});
 
-	// === REFS ===
-
-	const mapReady = ref(false);
-
-	function init(initConfig = {}) {
-		const parsedConfig = deepMerge(structuredClone(defaultConfig), initConfig);
-
-		// Set config
-		for (const key in parsedConfig) {
-			config[key] = parsedConfig[key];
-		}
-	}
-
 	return {
-		init,
+		// State
+
 		config,
 		state,
 		mapReady,
+
+		// Getters
+
+		// Actions
+		init,
 	};
 });
