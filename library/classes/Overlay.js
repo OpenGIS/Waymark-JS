435caa358dcf40c1608a63108fa3490f67fa67aa
Joe Hawes <morehawes@gmail.com>
2025-06-02 13:47:18 -0700

    6

--- library/classes/Overlay.js
+++ library/classes/Overlay.js
@@ -8,10 +8,23 @@ import { makeKey } from "@/helpers/Common.js";
 
 export class Overlay {
   constructor(feature) {
+    this.feature = feature;
     this.featureType = getFeatureType(feature);
-    this.typeKey = makeKey(feature.properties.type);
-    this.typeData = getTypeData(this.featureType, this.typeKey);
-    this.hasImage = featureHasImage(feature);
+    this.typeKey = makeKey(feature.properties.type) || "photo";
+    this.title = feature.properties.title || "";
+    this.description = feature.properties.description || "";
     this.images = getFeatureImages(feature);
+
+    this.typeData = getTypeData(this.featureType, this.typeKey);
+
+    this.hasImage = featureHasImage(feature);
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getDescription() {
+    return this.description;
   }
 }
