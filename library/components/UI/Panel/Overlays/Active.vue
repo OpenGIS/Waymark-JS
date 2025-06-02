035f31fa639323e35011c5df0720c6532b505770
Joe Hawes <morehawes@gmail.com>
2025-06-02 10:39:30 -0700

    2 - Layout & Elevation

--- library/components/UI/Panel/Overlays/Active.vue
+++ library/components/UI/Panel/Overlays/Active.vue
@@ -11,40 +11,56 @@ const { activeLayer } = storeToRefs(useInstanceStore());
 <template>
   <!-- START Active Layer -->
   <div v-if="activeLayer" class="active-layer">
-    <!-- Type Preview -->
-    <div class="type">
-      <Preview
-        :featureType="activeLayer.featureType"
-        :typeData="activeLayer.typeData"
+    <!-- START Top -->
+    <div class="layer-top">
+      <!-- Type Preview -->
+      <div class="type">
+        <Preview
+          :featureType="activeLayer.featureType"
+          :typeData="activeLayer.typeData"
+        />
+      </div>
+
+      <!-- Title -->
+      <div class="title">{{ activeLayer.feature.properties.title }}</div>
+
+      <Button icon="ion-close" @click="activeLayer = null" />
+    </div>
+    <!-- End Top -->
+
+    <!-- START Content -->
+    <div class="layer-content">
+      <!-- Image -->
+      <div class="image">
+        <img
+          v-if="activeLayer.feature.properties.image_medium_url"
+          :src="activeLayer.feature.properties.image_medium_url"
+        />
+      </div>
+
+      <!-- Coordinates -->
+      <div class="coordinates">
+        {{ activeLayer.feature.geometry.coordinates[1].toFixed(5) }},
+        {{ activeLayer.feature.geometry.coordinates[0].toFixed(5) }}
+      </div>
+
+      <!-- Elevation -->
+      <div
+        class="elevation"
+        v-if="activeLayer.feature.geometry.coordinates[2]"
+        v-html="
+          `Elevation: ${activeLayer.feature.geometry.coordinates[2].toFixed(2)} m`
+        "
+      />
+
+      <!-- Description -->
+      <div
+        class="description"
+        v-if="activeLayer.feature.properties.description"
+        v-html="activeLayer.feature.properties.description"
       />
     </div>
-
-    <!-- Title -->
-    <div class="title">{{ activeLayer.feature.properties.title }}</div>
-
-    <!-- Image -->
-    <div class="image">
-      <img
-        v-if="activeLayer.feature.properties.image_medium_url"
-        :src="activeLayer.feature.properties.image_medium_url"
-      />
-    </div>
-
-    <!-- Coordinates -->
-    <div class="coordinates">
-      {{ activeLayer.feature.geometry.coordinates[1].toFixed(5) }},
-      {{ activeLayer.feature.geometry.coordinates[0].toFixed(5) }}
-    </div>
-
-    <!-- Description -->
-    <div
-      class="description"
-      v-if="activeLayer.feature.properties.description"
-      v-html="activeLayer.feature.properties.description"
-    />
-
-    <!-- Close -->
-    <Button icon="ion-close" @click="activeLayer = null" />
+    <!-- End Content -->
   </div>
   <!-- End Active Layer -->
 </template>
@@ -58,46 +74,71 @@ const { activeLayer } = storeToRefs(useInstanceStore());
 
   border-bottom: 1px solid #999;
 
-  > div {
+  /* Top */
+  .layer-top {
     margin-bottom: 5px;
-  }
+    min-height: 34px;
+    display: flex;
+    align-items: center;
+    position: relative;
 
-  .type {
-    float: left;
-    margin-right: 10px;
-    width: 30px;
-  }
+    .type {
+      position: absolute;
+      top: 0;
+      left: 0;
 
-  .title {
-    padding-right: 30px;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 10px;
-  }
+      width: 30px;
+    }
 
-  .coordinates {
-    // font-size: 14px;
-    color: #666;
-    // margin-bottom: 10px;
-  }
-
-  .image {
-    float: left;
-    margin-right: 10px;
-    img {
-      max-width: 120px;
+    .title {
+      padding: 0 35px;
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .button {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: 0;
     }
   }
 
-  .description {
-    margin-bottom: 10px;
-    font-size: 14px;
-  }
+  /* Content */
+  .layer-content {
+    /* Self Clear floats */
+    height: auto;
+    clear: both;
 
-  .button {
-    position: absolute;
-    top: 2px;
-    right: 5px;
+    border: 1px solid blue;
+
+    > div {
+      margin-bottom: 5px;
+    }
+
+    .coordinates {
+      color: #666;
+    }
+
+    .image {
+      float: left;
+      margin-right: 5px;
+      margin-bottom: 5px;
+
+      img {
+        max-width: 129px;
+      }
+    }
+
+    .description {
+      margin-bottom: 10px;
+      font-size: 14px;
+    }
+
+    .button {
+      position: absolute;
+      top: 2px;
+      right: 5px;
+    }
   }
 }
 </style>
