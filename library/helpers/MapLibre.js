7589f8cf88dbca13df7d329f6a621a213c9ceffd
Joe Hawes <morehawes@gmail.com>
2025-10-07 09:44:09 -0700

    Use Bright style by default

--- library/helpers/MapLibre.js
+++ library/helpers/MapLibre.js
@@ -15,6 +15,6 @@ export const mapOptions = {
   center: [-1.8261632, 51.1788144], // Default to Stonehenge
   zoom: 14,
   maxZoom: 18,
-  style: "https://tiles.openfreemap.org/styles/liberty",
+  style: "https://tiles.openfreemap.org/styles/bright",
   attributionControl: false,
 };
